d5c1b6ac6b6f596205051a4452c08ba21499729b
IlyasSmirat <{ID}+{username}@users.noreply.github.com>
2026-09-10 12:01:21 +0200

    §

---
+++ day02/exercices/exercice-11.js
@@ -0,0 +1,26 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 02 · EXERCICE 11 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * PUISSANCE MANUELLE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Calculez 2 à la puissance 10 en utilisant une boucle for (sans utiliser Math.pow ni ).
+ *
+ * RÉSULTAT ATTENDU
+ * 1024
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-11
+ * ▶️ Commande : node day02/exercices/exercice-11.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+var n = 2
+var result = 1
+for(var i = 1; i <= 10; i++)
+    {
+       result *= n; 
+    } 
+    console.log(result)
---
+++ day02/exercices/exercice-13.js
@@ -0,0 +1,23 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 02 · EXERCICE 13 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * SUITE DE FIBONACCI
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Générez et affichez les 10 premiers nombres de la suite de Fibonacci. (La suite commence par 0, 1. Ensuite, chaque nombre est la somme des deux précédents : 0, 1, 1, 2, 3, 5, 8, 13...).
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-13
+ * ▶️ Commande : node day02/exercices/exercice-13.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+function fibonacci(n)
+{
+    if(n === 0) return 0
+    if (n === 1) return 1
+     return fibonacci(n - 1) + fibonacci (n - 2)
+}
+console.log(fibonacci(10))
